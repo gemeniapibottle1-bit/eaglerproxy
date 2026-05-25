2ff4cc0766ca7f3afcf94a09b95912c4f36c6678
gemeniapibottle1-bit <gemeniapibottle1@gmail.com>
2026-05-25 00:08:48 -0400

    Default backend to mc.hypixel.net:25565

--- src/config.ts
+++ src/config.ts
@@ -46,7 +46,7 @@ export const config: Config = {
       originBlacklist: null,
     },
     server: {
-      host: process.env.SERVER_HOST ?? "127.0.0.1",
+      host: process.env.SERVER_HOST ?? "mc.hypixel.net",
       port: parseInt(process.env.SERVER_PORT ?? "25565"),
     },
     tls: undefined,
